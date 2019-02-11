af4c5562a265a0eaba986ede57214e53238f9d74
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-02-10 20:09:50 -0600

    Completed a working version of the game.js. If correct letter is selected on last guess it is recorded as a loss creating a false negative that needs to be remedied in the document.onkeyup function block.

--- javascript/game.js
+++ javascript/game.js
@@ -1,62 +1,80 @@
-var compGuessArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Variable for the computer to randomly select a letter for the player to try and guess.
+var compChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// Game play variables start values
 var wins = 0;
 var losses = 0;
 var guesses = 5;
 var guessesLeft = 5;
 var guessedLetters = [];
-var letterToGuess = null;
+var compGuess = null;
 
-var compGuess = compGuessArray[Math.floor(Math.random()*compGuessArray.length)];
+// Variable that holds the random computer selection
+// var compGuess = compChoices[Math.floor(Math.random() * compChoices.length)];
 
+
+//Allows the player to guess # of times based on the guesses variable value
 function updateGuessesLeft() {
     document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
 };
 
-function updateLetterToGuess() {
-    this.letterToGuess = this.compGuessArray[Math.floor(Math.random() * this.compGuessArray.length)];
+//Selects a new letter at random for the player to try and guess.
+function updatecompGuess() {
+    this.compGuess = this.compChoices[Math.floor(Math.random() * this.compChoices.length)];
 };
 
+//Displays wrong letters guessed in a round
 function updateGuessesSoFar() {
     document.querySelector("#guessesSoFar").innerHTML = "Your Guesses So Far: " + guessedLetters.join(", ");
 };
 
+//Reset at the end of a round
 var reset = function() {
+    totalGuesses = 5;
     guessesLeft = 5;
     guessedLetters = [];
 
-    updateLetterToGuess();
+    updatecompGuess();
     updateGuessesLeft();
     updateGuessesSoFar();
 }
 
-updateLetterToGuess();
+//Calls the functions
+updatecompGuess();
 updateGuessesLeft();
 
-document.onKeyup = function(event) {
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    var check = compGuessArray.includes(userGuess);
+//compares the player's input with the array of possible choices.
+document.onkeyup = function(event) {
+    var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    var check = compChoices.includes(playerGuess);
+    // console.log(playerGuess);
 
+    //alerts the player of an invalid input selection
     if (check === false) {
+        alert("That is not a valid guess. Pick a letter from the alphabet.");
         return false;
+
+    //
     } else if (check === true) {
         guessesLeft--;
-        guessedLetters.push(userGuess);
+        guessedLetters.push(playerGuess);
         updateGuessesLeft();
         updateGuessesSoFar();
 
         if (guessesLeft > 0) {
-            if (userGuess == letterToGuess) {
+            if (playerGuess == compGuess) {
                 wins++;
                 document.querySelector("#wins").innerHTML = "Wins: " + wins;
-                userGuess = userGuess.toUpperCase();
+                playerGuess = playerGuess.toUpperCase();
                 reset();
             }
-        } else if (guessesLeft === 0) {
+        } else if (guessesLeft == 0) {
             losses++;
             document.querySelector("#losses").innerHTML = "Losses: " + losses;
             reset();
         }
         return false;
+    } else {
+        alert("An error has occurred.")
     }
 };
